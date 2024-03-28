b63747d569ef2ca0aac973c1f92976da2a0bd5b3
Israiloff <stardustindark@gmail.com>
2024-03-28 12:34:42 +0500

    fixed: latest tag related build script issue;

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 BUILD_VERSION=0.0.1
 docker build -t israiloff/alpine-docker:$BUILD_VERSION .
 docker tag israiloff/alpine-docker:$BUILD_VERSION 
-docker tag israiloff/alpine-docker:latest
+docker tag israiloff/alpine-docker:$BUILD_VERSION israiloff/alpine-docker:latest
